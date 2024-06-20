d01dc11482c28734779bf9f7555f12907814da12
tejaswinunna14 <tejaswinunna14@gmail.com>
2024-06-20 09:52:02 +0530

    first commit

---
+++ app1/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+def srujana(request):
+    return HttpResponse("<h1><marquee>srujana tinnavara, Era vadilestunava..............</marquee></h1>")
